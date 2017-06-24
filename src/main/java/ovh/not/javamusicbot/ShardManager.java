6dd99dd5eea46495bdd00ecd3fd6b9476f138f0b
SpongyBacon <spongybacon@hotmail.co.uk>
2017-06-24 22:13:45 +0100

    remove jda-nas, update jda

--- src/main/java/ovh/not/javamusicbot/ShardManager.java
+++ src/main/java/ovh/not/javamusicbot/ShardManager.java
@@ -1,6 +1,5 @@
 package ovh.not.javamusicbot;
 
-import com.sedmelluq.discord.lavaplayer.jdaudp.NativeAudioSendFactory;
 import net.dv8tion.jda.core.AccountType;
 import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.JDABuilder;
@@ -78,7 +77,6 @@ public class ShardManager {
             CommandManager commandManager = new CommandManager(config, constants, this);
             JDABuilder builder = new JDABuilder(AccountType.BOT)
                     .setToken(config.token)
-                    .setAudioSendFactory(new NativeAudioSendFactory())
                     .addEventListener(new Listener(config, commandManager, this));
             if (sharding) {
                 builder.useSharding(id, shardCount);
