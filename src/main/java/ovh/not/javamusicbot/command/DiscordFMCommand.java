0b57719c6c38cabe67290b04b862a08c8270e540
Joe Burnard <spongybacon@hotmail.co.uk>
2017-08-31 01:07:00 +0100

    Use local discord.fm playlist files instead of temp.discord.fm (#66)
    * add discord.fm playlist files
    
    * use dfm playlist files instead of temp.discord.fm

--- src/main/java/ovh/not/javamusicbot/command/DiscordFMCommand.java
+++ src/main/java/ovh/not/javamusicbot/command/DiscordFMCommand.java
@@ -4,23 +4,25 @@ import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
 import net.dv8tion.jda.core.Permission;
 import net.dv8tion.jda.core.entities.Message;
 import net.dv8tion.jda.core.entities.VoiceChannel;
-import okhttp3.Request;
-import okhttp3.Response;
+import net.dv8tion.jda.core.utils.IOUtil;
 import org.json.JSONArray;
-import org.json.JSONObject;
-import ovh.not.javamusicbot.*;
+import ovh.not.javamusicbot.Command;
+import ovh.not.javamusicbot.CommandManager;
+import ovh.not.javamusicbot.GuildMusicManager;
+import ovh.not.javamusicbot.LoadResultHandler;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @SuppressWarnings("ConstantConditions")
 public class DiscordFMCommand extends Command {
-    private static final String DFM_BASE_URL = "http://temp.discord.fm";
-    private static final String DFM_LIBRARIES_URL = DFM_BASE_URL + "/libraries/json";
-    private static final String DFM_LIBRARY_URL = DFM_BASE_URL + "/libraries/%s/json";
+    private static final String DFM_DIRECTORY_PATH = "discordfm/";
 
     private final CommandManager commandManager;
     private final AudioPlayerManager playerManager;
-    private Library[] libraries = null;
+    private Collection<Library> libraries = null;
     private String usageResponse = null;
 
     public DiscordFMCommand(CommandManager commandManager, AudioPlayerManager playerManager) {
@@ -30,32 +32,26 @@ public class DiscordFMCommand extends Command {
     }
 
     private void load() {
-        try {
-            Request request = new Request.Builder().url(DFM_LIBRARIES_URL).build();
-            Response response = MusicBot.HTTP_CLIENT.newCall(request).execute();
+        libraries = Arrays.stream(new File(DFM_DIRECTORY_PATH).listFiles())
+                .map(file -> new Library(file.getName(), file))
+                .sorted(Comparator.comparing(o -> o.name))
+                .collect(Collectors.toCollection(ArrayList::new));
 
-            JSONArray array = new JSONArray(response.body().string());
-            response.close();
+        StringBuilder builder = new StringBuilder("Uses a song playlist from http://discord.fm\nUsage: `%prefix%dfm <library>`" +
+                "\n\n**Available libraries:**\n");
 
-            this.libraries = new Library[array.length()];
-            for (int i = 0; i < array.length(); i++) {
-                JSONObject object = array.getJSONObject(i);
-                libraries[i] = new Library(object);
+        Iterator<Library> iterator = libraries.iterator();
+
+        while (iterator.hasNext()) {
+            Library library = iterator.next();
+            builder.append(library.name);
+
+            if (iterator.hasNext()) {
+                builder.append(", ");
             }
-            StringBuilder builder = new StringBuilder("Uses a song playlist from http://discord.fm\nUsage: `%prefix%dfm <library>`" +
-                    "\n\n**Available libraries:**\n");
-            for (int i = 0; i < libraries.length; i++) {
-                builder.append(libraries[i].name);
-                if (i != libraries.length - 1) {
-                    builder.append(", ");
-                }
-            }
-            this.usageResponse = builder.toString();
-        } catch (Exception e) {
-            e.printStackTrace();
-            this.libraries = null;
-            this.usageResponse = null;
         }
+
+        usageResponse = builder.toString();
     }
 
     @Override
@@ -65,15 +61,18 @@ public class DiscordFMCommand extends Command {
             context.reply("You must be in a voice channel!");
             return;
         }
+
         if (libraries == null || usageResponse == null) {
             Message msg = context.reply("Loading libraries..");
             load();
             msg.delete().queue();
         }
+
         if (context.args.length == 0) {
             context.reply(usageResponse);
             return;
         }
+
         GuildMusicManager musicManager = GuildMusicManager.getOrCreate(context.event.getGuild(),
                 context.event.getTextChannel(), playerManager);
         if (musicManager.open && musicManager.player.getPlayingTrack() != null
@@ -83,69 +82,74 @@ public class DiscordFMCommand extends Command {
                     "be moved. Members with the `VOICE_MOVE_OTHERS` permission are exempt from this.");
             return;
         }
+
         String libraryName = String.join(" ", context.args);
-        Library library = null;
-        for (Library lib : libraries) {
-            if (lib != null && lib.name.equalsIgnoreCase(libraryName)) {
-                library = lib;
-                break;
-            }
-        }
-        if (library == null) {
+
+        Optional<Library> library = libraries.stream()
+                .filter(lib -> lib != null && lib.name.equalsIgnoreCase(libraryName))
+                .findFirst();
+
+        if (!library.isPresent()) {
             context.reply("Invalid library! Use `%prefix%dfm` to see usage & libraries.");
             return;
         }
+
         String[] songs;
         try {
-            songs = library.songs();
+            songs = library.get().getSongs();
         } catch (IOException e) {
             e.printStackTrace();
             context.reply("An error occurred!");
             return;
         }
+
         if (songs == null) {
             context.reply("An error occurred!");
             return;
         }
+
         musicManager.scheduler.queue.clear();
         musicManager.scheduler.repeat = false;
         musicManager.scheduler.loop = false;
         musicManager.player.stopTrack();
+
         LoadResultHandler handler = new LoadResultHandler(commandManager, musicManager, playerManager, context);
         handler.verbose = false;
+
         for (String song : songs) {
             playerManager.loadItem(song, handler);
         }
+
         if (!musicManager.open) {
             musicManager.open(channel, context.event.getAuthor());
         }
     }
 
     private class Library {
-        private String id, name;
+        private final String name;
+        private final File file;
 
-        private Library(JSONObject json) {
-            try {
-                this.id = json.getString("id");
-                this.name = json.getString("name");
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+        private String[] songs = null;
+
+        private Library(String name, File file) {
+            name = name.replace("_", " ");
+            this.name = name.substring(0, name.length() - 5);
+            this.file = file;
         }
 
-        private String[] songs() throws IOException {
-            String url = String.format(DFM_LIBRARY_URL, id);
+        private String[] getSongs() throws IOException {
+            if (songs != null) {
+                return songs;
+            }
 
-            Request request = new Request.Builder().url(url).build();
-            Response response = MusicBot.HTTP_CLIENT.newCall(request).execute();
-
-            JSONArray array = new JSONArray(response.body().string());
-            response.close();
+            JSONArray array = new JSONArray(new String(IOUtil.readFully(file)));
 
             String[] songs = new String[array.length()];
             for (int i = 0; i < array.length(); i++) {
                 songs[i] = array.getJSONObject(i).getString("identifier");
             }
+
+            this.songs = songs;
             return songs;
         }
     }
